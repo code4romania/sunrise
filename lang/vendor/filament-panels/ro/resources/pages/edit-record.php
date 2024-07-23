1a8c25a02b1d860bc0e2a27442c45aa94da3d09a
Alex Popa <alex.popa@commitglobal.org>
2024-07-24 00:08:24 +0300

    translate form actions

---
+++ lang/vendor/filament-panels/ro/resources/pages/edit-record.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'title' => 'Editare :label',
+
+    'breadcrumb' => 'Editare',
+
+    'form' => [
+
+        'actions' => [
+
+            'cancel' => [
+                'label' => 'Renunță',
+            ],
+
+            'save' => [
+                'label' => 'Salvează modificările',
+            ],
+
+        ],
+
+    ],
+
+    'content' => [
+
+        'tab' => [
+            'label' => 'Editare',
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'saved' => [
+            'title' => 'Salvat cu succes',
+        ],
+
+    ],
+
+];
