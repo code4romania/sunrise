01ff6d3a920580e93cf015010047cf67f852ffa7
alexPopaCode4 <156655986+alexPopaCode4@users.noreply.github.com>
2024-09-13 11:20:35 +0300

    Close file (#145)
    ---------
    
    Co-authored-by: Andrei Ioniță <hi@andrei.io>

--- app/Filament/Organizations/Resources/BeneficiaryResource/Pages/ViewBeneficiary.php
+++ app/Filament/Organizations/Resources/BeneficiaryResource/Pages/ViewBeneficiary.php
@@ -286,6 +286,7 @@ class ViewBeneficiary extends ViewRecord
         return [
             BeneficiaryResource\Widgets\CaseTeamListWidget::class,
             BeneficiaryResource\Widgets\DocumentsListWidget::class,
+            BeneficiaryResource\Widgets\CloseFileWidget::class,
         ];
     }
 }
---
+++ database/factories/CloseFileFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Enums\AdmittanceReason;
+use App\Enums\CloseMethod;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CloseFile>
+ */
+class CloseFileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $closeMethod = $this->faker->randomElement(CloseMethod::values());
+
+        return [
+            'date' => $this->faker->date(),
+            'number' => $this->faker->randomNumber(),
+            'admittance_date' => $this->faker->date(),
+            'exit_date' => $this->faker->date(),
+            'admittance_reason' => $this->faker->randomElements(AdmittanceReason::values(), rand(0, 5)),
+            'admittance_details' => $this->faker->text(100),
+            'close_method' => $closeMethod,
+            'institution_name' => CloseMethod::isValue($closeMethod, CloseMethod::TRANSFER_TO) ? $this->faker->text(100) : null,
+            'beneficiary_request' => CloseMethod::isValue($closeMethod, CloseMethod::BENEFICIARY_REQUEST) ? $this->faker->text(100) : null,
+            'other_details' => CloseMethod::isValue($closeMethod, CloseMethod::OTHER) ? $this->faker->text(100) : null,
+            'close_situation' => $this->faker->text,
+        ];
+    }
+}
