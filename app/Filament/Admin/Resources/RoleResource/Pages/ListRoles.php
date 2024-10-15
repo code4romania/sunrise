f9227cb19e112fe75ce4f119232292d88bdafbfe
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-10-15 17:02:55 +0300

    wip

--- app/Filament/Admin/Resources/RoleResource/Pages/ListRoles.php
+++ app/Filament/Admin/Resources/RoleResource/Pages/ListRoles.php
@@ -7,10 +7,13 @@ namespace App\Filament\Admin\Resources\RoleResource\Pages;
 use App\Filament\Admin\Resources\RoleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ManageRecords;
 
-class ListRoles extends ListRecords
+class ListRoles extends ManageRecords
 {
     protected static string $resource = RoleResource::class;
+
+    protected static string $view = 'filament.admin.pages.nomenclature-list';
 
     protected function getHeaderActions(): array
     {
--- app/Filament/Admin/Resources/ServiceResource/Pages/ManageServices.php
+++ app/Filament/Admin/Resources/ServiceResource/Pages/ManageServices.php
@@ -6,11 +6,17 @@ namespace App\Filament\Admin\Resources\ServiceResource\Pages;
 
 use App\Filament\Admin\Resources\ServiceResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ManageRecords;
+use Illuminate\Contracts\View\View;
 
 class ManageServices extends ManageRecords
 {
     protected static string $resource = ServiceResource::class;
+
+    protected static string $view = 'filament.admin.pages.nomenclature-list';
+
+
 
     protected function getHeaderActions(): array
     {
@@ -21,4 +27,13 @@ class ManageServices extends ManageRecords
                 ->createAnother(false),
         ];
     }
+
+//    public function getCustomTabs(): array
+//    {
+//        return [
+//            ['label' => 'Tab1', 'url'=> 'tab1'],
+//            ['label' => 'Tab2', 'url'=> 'tab2'],
+//            ['label' => 'Tab3', 'url'=> 'tab3'],
+//        ];
+//    }
 }
