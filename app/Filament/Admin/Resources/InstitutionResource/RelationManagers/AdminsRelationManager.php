7a58ddb7a2f0e07ef9d0193b0aebf0adaad506fe
Alex Popa <alex.popa@commitglobal.org>
2024-10-30 23:37:17 +0200

    super admin institutions

---
+++ app/Filament/Admin/Resources/InstitutionResource/RelationManagers/AdminsRelationManager.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Admin\Resources\InstitutionResource\RelationManagers;
+
+use App\Filament\Admin\Resources\InstitutionResource;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'admins';
+
+    public function isReadOnly(): bool
+    {
+        return false;
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('first_name')
+                    ->label(__('institution.labels.first_name')),
+
+                TextInput::make('last_name')
+                    ->label(__('institution.labels.last_name')),
+
+                TextInput::make('email')
+                    ->label(__('institution.labels.email')),
+
+                TextInput::make('phone')
+                    ->label(__('institution.labels.phone')),
+
+                Hidden::make('ngo_admin')
+                    ->default(1),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->heading(__('institution.headings.admin_users'))
+            ->columns([
+                TextColumn::make('first_name')
+                    ->label(__('institution.labels.first_name')),
+
+                TextColumn::make('last_name')
+                    ->label(__('institution.labels.last_name')),
+
+                TextColumn::make('roles')
+                    ->label(__('institution.labels.roles')),
+
+                TextColumn::make('status')
+                    ->label(__('institution.labels.account_status')),
+
+                TextColumn::make('last_login_at')
+                    ->label(__('institution.labels.last_login_at')),
+            ])
+            ->headerActions([
+                CreateAction::make()
+                    ->label(__('institution.actions.add_ngo_admin'))
+                    ->modalHeading(__('institution.actions.add_ngo_admin'))
+                    ->createAnother(false),
+            ])
+            ->actions([
+                ViewAction::make()
+                    ->label(__('general.action.view_details'))
+                    ->url(
+                        fn ($record) => InstitutionResource::getUrl('user.view', [
+                            'parent' => $this->getOwnerRecord(),
+                            'record' => $record,
+                        ])
+                    ),
+            ]);
+    }
+
+    public static function getTitle(Model $ownerRecord, string $pageClass): string
+    {
+        return __('institution.headings.ngo_admin');
+    }
+}
