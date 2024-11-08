4a5d20ee96dfc99a82ef594a36eff925d455f9f2
Alex Popa <alex.popa@commitglobal.org>
2024-11-08 10:33:45 +0200

    Beneficiary intervention view and edit page

---
+++ app/Forms/Components/DatePicker.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Forms\Components;
+
+class DatePicker extends \Filament\Forms\Components\DatePicker
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->native(false);
+    }
+}
