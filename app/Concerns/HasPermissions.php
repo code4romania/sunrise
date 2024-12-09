fe04de2007b8dc1c837454d3c4a6c4613c17a7c2
Alex Popa <alex.popa@commitglobal.org>
2024-12-09 02:27:25 +0200

    Beneficiary history policy

--- app/Concerns/HasPermissions.php
+++ app/Concerns/HasPermissions.php
@@ -67,7 +67,7 @@ trait HasPermissions
         return (bool) $this->permissions?->admin_permissions->contains(AdminPermission::CAN_CHANGE_NOMENCLATURE);
     }
 
-    public function hasAccessToCommunity()
+    public function hasAccessToCommunity(): bool
     {
         if ($this->isAdmin()) {
             return true;
