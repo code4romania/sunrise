17c1faa777a43d91e5743bbe37e291080e0a5c9a
Alex Popa <alex.popa@commitglobal.org>
2025-01-10 22:53:16 +0200

    Change label for organizations card from dashboard

--- lang/ro/admin.php
+++ lang/ro/admin.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return[
     'stats' => [
         'total_institutions' => 'Instituții',
-        'total_organizations' => 'Organizații',
+        'total_organizations' => 'Centre',
         'beneficiaries_total_cases' => 'Cazuri totale',
         'beneficiaries_open_cases' => 'Cazuri deschise',
     ],
