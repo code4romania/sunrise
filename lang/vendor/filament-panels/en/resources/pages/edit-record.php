acaf6cd920c42133392fe17717fe48071cdc9d32
Weblate <noreply@weblate.org>
2025-03-13 16:25:34 +0000

    Update translation files
    Updated by "Remove blank strings" add-on in Weblate.
    
    Update translation files
    
    Updated by "Squash Git commits" add-on in Weblate.
    
    Update translation files
    
    Updated by "Remove blank strings" add-on in Weblate.
    
    Update translation files
    
    Updated by "Squash Git commits" add-on in Weblate.
    
    Update translation files
    
    Updated by "Squash Git commits" add-on in Weblate.
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.commitglobal.org/projects/sunrise/create-record/
    Translate-URL: https://translate.commitglobal.org/projects/sunrise/create/
    Translate-URL: https://translate.commitglobal.org/projects/sunrise/user/
    Translation: Sunrise/create
    Translation: Sunrise/create-record
    Translation: Sunrise/user

---
+++ lang/vendor/filament-panels/en/resources/pages/edit-record.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'title' => 'editing:label',
+    'breadcrumb' => 'Edit',
+    'form' => [
+        'actions' => [
+            'cancel' => [
+                'label' => 'quit',
+            ],
+            'save' => [
+                'label' => 'Save changes',
+            ],
+        ],
+    ],
+    'content' => [
+        'tab' => [
+            'label' => 'Edit',
+        ],
+    ],
+    'notifications' => [
+        'saved' => [
+            'title' => 'Successfully saved',
+        ],
+    ],
+];
