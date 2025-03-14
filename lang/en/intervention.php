07d886c401292a341693f241c187c605a2e22ee8
Weblate <noreply@weblate.org>
2025-03-14 17:05:18 +0000

    Update translation files
    Updated by "Remove blank strings" add-on in Weblate.
    
    Translation: Sunrise/intervention
    Translate-URL: https://translate.commitglobal.org/projects/sunrise/intervention/

--- lang/en/intervention.php
+++ lang/en/intervention.php
@@ -1,15 +1,1 @@
 <?php
-return [
-    'label' => [
-        'singular' => '',
-        'plural' => '',
-    ],
-    'field' => [
-        'name' => '',
-        'description' => '',
-        'use_count' => '',
-    ],
-    'action' => [
-        'create' => '',
-    ],
-];
